213c344e2e3ebe43ee383d226547402a67fcba97
Benjamin Geib <br.geib33@gmail.com>
2018-04-08 13:33:40 -0400

    Update network_measures_script_AAL_Average_Ben.m

--- network_measures_script_AAL_Average_Ben.m
+++ network_measures_script_AAL_Average_Ben.m
@@ -29,28 +29,42 @@ for i = 1
     
     % ====== I added this, according to what I understood from you.....
     nbeta=numel(SPM.Vbeta);
+    % so nbeta is your 'time' dimension, we gotta make your data 4d before we can do anything
+    c=1; % need a counter for this style
     for i_beta=1:nbeta
+        % see n2sp(number,padding) is simpler imo e.g. n2sp(1,4) produces the string "0001"
        if i_beta < 10
-          data_path = strcat('/data/projects/NeFF_B5/Real_Study/Forgetting/Basic/S', num2str(i), '/analysis/DF_GT/beta_000', num2str(i_beta),'.nii');
+          data_path{i_beta} = strcat('/data/projects/NeFF_B5/Real_Study/Forgetting/Basic/S', num2str(i), '/analysis/DF_GT/beta_000', num2str(i_beta),'.nii');
        else
-          data_path = strcat('/data/projects/NeFF_B5/Real_Study/Forgetting/Basic/S', num2str(i), '/analysis/DF_GT/beta_00', num2str(i_beta),'.nii'); 
+          data_path{i_beta} = strcat('/data/projects/NeFF_B5/Real_Study/Forgetting/Basic/S', num2str(i), '/analysis/DF_GT/beta_00', num2str(i_beta),'.nii'); 
        end
+       % data_path should be a cell structure imo, depends how you wanna implement and what knowledge you wanna save
        
         name_beta=SPM.Vbeta(i_beta).descrip;
-        process_set=name_beta(29:30);
+        process_set=name_beta(29:30); % explicit references are safer imo e.g. some type of str matching
     % ======    
-        Y = spm_vol(data_path); data=spm_read_vols(Y);
-%         for jj=1:length(data_path)
-%             Y = spm_vol(data_path{jj}); 
-%              data(:,:,:,i_beta)=spm_read_vols(Y);
-%         end
+    if strcmp(process_set,condition) % condition needs defined in 2nd loop
+    
+       Y = spm_vol(data_path{i_beta}); data=spm_read_vols(Y);
+        data(:,:,:,c)=spm_read_vols(Y); c=c+1; % increment counter when condition found
+        % This loop is essential to making the 4d file! its gotta stay in
+        % for jj=1:length(data_path)
+         %    Y = spm_vol(data_path{jj}); 
+         %     data(:,:,:,i_beta)=spm_read_vols(Y);
+        %end
+        
+        % if you're looping through all nbeta you're gonna get constants and motion parameters too, which I don't think you want
+        
+    end % of nbeta loop! we need a 4d file before we can do any network stuff
 
-
+    % now data is [x,y,z,time] matrix and we shouldn't need to modify any of these scripts
         [adj_matrix, c,l,e,Q,Ci, c_rand, l_rand, e_rand, Q_rands, Ci_rand, degree, bet, thr, degree_rand, bet_rand] = network_measures_aal_newman_Ben(data, aal);
         clearvars -except adj_matrix i c l e Q Ci c_rand l_rand e_rand Q_rands Ci_rand degree bet thr degree_rand bet_rand aal ids num id
         
         % ======
         % ====== I added this, according to what I understood from you.....
+        % this confuses me? i think this loop should be 2nd e.g. subject loop, condition loop, vbeta loop
+        
         switch process_set
             %SPM.Vbeta(1) from here get the conditions
         case 'NR'
@@ -66,7 +80,8 @@ for i = 1
         % ======
         
         save(save_path)
-    end
+        
+    % end should be sooner, must be sooner
         
   
  
